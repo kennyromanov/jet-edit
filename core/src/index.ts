fd14097fd0e6062ad968a036025faa3a25dc709b
Kenny Romanov <107574726+kennyromanov@users.noreply.github.com>
2025-10-26 08:47:15 +0500

    upd: The project has been splitted into 'core' and 'app' in a monorepo

---
+++ core/src/index.ts
@@ -0,0 +1,63 @@
+import { createApp } from 'vue';
+import { Obj } from '@/types';
+import { useI18nCache, useI18nReactivity } from '@/i18n/lib';
+import store from '@/pinia/store';
+import i18n from '@/i18n';
+import router from '@/router';
+import Editor from '@/components/Editor.vue';
+import App from './App.vue';
+import './index.css';
+
+
+// Third-parties
+
+export const appStore = store.useApp();
+
+
+// Constants
+
+export const MOBILE_WIDTH = 1024;
+
+
+// Functions
+
+export function updIsMobile(): void {
+    appStore.setIsMobile(window.innerWidth < MOBILE_WIDTH);
+}
+
+export function createEditor(el: string | HTMLElement): Obj {
+
+    // Getting the cache
+
+    useI18nCache();
+
+    useI18nReactivity();
+
+
+    // Setting the listeners
+
+    window.addEventListener('resize', updIsMobile);
+
+
+    // Defining the functions
+
+    const clear = () => window.removeEventListener('resize', updIsMobile);
+
+
+    // Initializing the app
+
+    updIsMobile();
+
+    createApp(App)
+        .use(i18n)
+        .use(router)
+        .mount(el);
+
+
+    return { clear };
+}
+
+
+export default createEditor;
+
+export { Editor };
--- core/vite.config.ts
+++ core/vite.config.ts
@@ -5,21 +5,31 @@ import tailwindcss from '@tailwindcss/vite';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    eslint(),
-  ],
+  plugins: [ vue(), tailwindcss(), eslint() ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-      '@config': resolve(__dirname, './env.v.json'),
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
+      },
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'jet-edit',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: [ 'vue' ],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
       },
     },
   },
